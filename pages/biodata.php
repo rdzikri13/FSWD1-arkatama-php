2db1e9ca866900cd83ba3f70076d6fa616bad503
rdzikri13 <126127985+rdzikri13@users.noreply.github.com>
2023-04-18 00:39:21 +0700

    Update biodata.php

--- pages/biodata.php
+++ pages/biodata.php
@@ -11,22 +11,4 @@
 		$tempat_lahir = 'Tasikmalaya';
 		$tanggal_lahir = '04 Juli 2002';
 		$jenis_kelamin = 'Pria';
-		$alamat = '';
-		$foto = ';
-		$pendidikan = 'S1 Pendidikan Sejarah- Universitas Pendidikan Indonesia';
-		$hobbies = 'Olahraga, Musik';
-
-		// TAMPILKAN BIODATA
-		echo '<h1>BIODATA</h1>';
-		echo '<div class="container">';
-        echo '<div class="photo"><img src="' . $foto . '" alt="Foto"></div>';
-		echo '<div class="row"><span class="label">Nama         :</span> <span class="value">' . $nama . '</span></div>';
-		echo '<div class="row"><span class="label">Tempat/Tanggal Lahir:</span> <span class="value">' . $tempat_lahir . ', ' . $tanggal_lahir . '</span></div>';
-		echo '<div class="row"><span class="label">Jenis Kelamin:</span> <span class="value">' . $jenis_kelamin . '</span></div>';
-		echo '<div class="row"><span class="label">Alamat       :</span> <span class="value">' . $alamat . '</span></div>';    
-		echo '<div class="row"><span class="label">Pendidikan   :</span> <span class="value">' . $pendidikan . '</span></div>';
-		echo '<div class="row"><span class="label">Hoby         :</span> <span class="value">' . $hobbies . '</span></div>';
-		echo '</div>';
-	?>
-</body>
-</html>
+	
